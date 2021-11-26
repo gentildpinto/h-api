7fcb8d12a58e106f610104a3ef02d7ef36884d6d
gentildpinto <gentil.junior.gp@gmail.com>
2021-11-26 22:34:11 +0100

    run server

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"os"
+
+	"github.com/gentildpinto/h-api/internal/server"
 	"github.com/gentildpinto/h-api/internal/tools/logger"
 )
 
@@ -12,5 +15,16 @@ func Run(appVersion string) (err error) {
 	if err = logger.Initialize(appVersion); err != nil {
 		return
 	}
+
+	if os.Getenv("PORT") != "" {
+		appPort = os.Getenv("PORT")
+	}
+
+	debug := os.Getenv("ENVIRONMENT") != "production" || os.Getenv("DEBUG") == "true"
+
+	s := server.New(appPort, 60, 60, debug)
+
+	s.Run()
+
 	return
 }
