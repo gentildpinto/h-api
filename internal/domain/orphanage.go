7f6ff319e51c92fd481ccd241400de0eba956549
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-11-30 11:01:43 +0100

    updated orphanage domain

--- internal/domain/orphanage.go
+++ internal/domain/orphanage.go
@@ -1,13 +1,15 @@
 package domain
 
+import "github.com/lib/pq"
+
 type Orphanage struct {
 	Base
-	Name           string   `json:"name"`
-	About          string   `json:"about"`
-	Latitude       float64  `json:"latitude"`
-	Longitude      float64  `json:"longitude"`
-	Instructions   string   `json:"instructions"`
-	OpendHours     string   `json:"opend_hours"`
-	OpenOnWeekends bool     `json:"open_on_weekends"`
-	Images         []string `json:"images"`
+	Name           string         `json:"name"`
+	About          string         `json:"about"`
+	Latitude       float64        `json:"latitude"`
+	Longitude      float64        `json:"longitude"`
+	Instructions   string         `json:"instructions"`
+	OpendHours     string         `json:"opend_hours"`
+	OpenOnWeekends bool           `json:"open_on_weekends"`
+	Images         pq.StringArray `json:"images" gorm:"type:text[]"`
 }
