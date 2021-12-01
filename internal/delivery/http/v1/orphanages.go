10bbd01bfc1f5736de3a6ce387ea06e1ad8e4643
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-12-01 13:53:40 +0100

    removed unnused payload

--- internal/delivery/http/v1/orphanages.go
+++ internal/delivery/http/v1/orphanages.go
@@ -7,17 +7,6 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type createOrphanagePayload struct {
-	Name           string   `json:"name" validate:"required"`
-	Latitude       float64  `json:"latitude" validate:"required"`
-	Longitude      float64  `json:"longitude" validate:"required"`
-	About          string   `json:"about" validate:"required"`
-	Instructions   string   `json:"instructions" validate:"required"`
-	OpenedHours    string   `json:"opened_hours" validate:"required"`
-	OpenOnWeekends bool     `json:"open_on_weekends" validate:"required"`
-	Images         []string `json:"images" validate:"required"`
-}
-
 func (h *Handler) initOrphanagesRoutes(e *echo.Group) {
 	orphanages := e.Group("/orphanages")
 	{
