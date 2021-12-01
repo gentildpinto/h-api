8945ded9954195ab1bcca04dd71340f25e952fdd
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-12-01 13:46:16 +0100

    removed lib pq package from orphanage domain

--- internal/domain/orphanage.go
+++ internal/domain/orphanage.go
@@ -1,15 +1,13 @@
 package domain
 
-import "github.com/lib/pq"
-
 type Orphanage struct {
 	Base
-	Name           string         `json:"name"`
-	About          string         `json:"about"`
-	Latitude       float64        `json:"latitude"`
-	Longitude      float64        `json:"longitude"`
-	Instructions   string         `json:"instructions"`
-	OpenedHours    string         `json:"opened_hours"`
-	OpenOnWeekends bool           `json:"open_on_weekends"`
-	Images         pq.StringArray `json:"images" gorm:"type:text[]"`
+	Name           string  `json:"name"`
+	About          string  `json:"about"`
+	Latitude       float64 `json:"latitude"`
+	Longitude      float64 `json:"longitude"`
+	Instructions   string  `json:"instructions"`
+	OpenedHours    string  `json:"opened_hours"`
+	OpenOnWeekends bool    `json:"open_on_weekends"`
+	Images         []Image `json:"images,omitempty"`
 }
