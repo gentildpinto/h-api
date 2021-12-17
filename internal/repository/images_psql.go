25ea787eaff37eb3328c2a67ee1229b9f8e2fb67
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-12-17 16:13:29 +0100

    implemented upload images to firebase

--- internal/repository/images_psql.go
+++ internal/repository/images_psql.go
@@ -1,46 +1,27 @@
 package repository
 
 import (
-	"io"
 	"mime/multipart"
-	"os"
-	"strings"
 
 	"github.com/gentildpinto/h-api/internal/domain"
+	"github.com/gentildpinto/h-api/pkg/storage"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 func createImages(tx *gorm.DB, ophID uuid.UUID, images []*multipart.FileHeader) (err error) {
 	for _, image := range images {
-		src, err := image.Open()
 
+		filename, err := storage.UploadImage(image)
 		if err != nil {
 			return err
 		}
 
-		defer src.Close()
-
-		filename := strings.Trim(strings.ToLower(image.Filename), " ")
-
-		filepath := "public/uploads/" + filename
-		imageurl := os.Getenv("APP_URL") + "/" + filepath
-
-		dst, err := os.Create(filepath)
-
-		if err != nil {
-			return err
-		}
-
-		defer dst.Close()
-
-		if _, err = io.Copy(dst, src); err != nil {
-			return err
-		}
+		filepath := "https://firebasestorage.googleapis.com/v0/b/happyngrb.appspot.com/o/images%2F" + filename + "?alt=media"
 
 		if err = tx.Create(&domain.Image{
 			OrphanageID: ophID,
-			Path:        imageurl,
+			Path:        filepath,
 		}).Error; err != nil {
 			return err
 		}
